c3566a592cfb8067a0f664387ab72246e83a402d
anikshajol <anikshajol@gmail.com>
2023-09-02 01:07:39 +0600

    project complete

--- js/index.js
+++ js/index.js
@@ -24,7 +24,6 @@ const showCategoryData = (categoryData) => {
 
     divChild.innerHTML = `
     <a onclick="handleClick(${category.category_id})" class="btn" id='active' >${category.category}</a>
-    
     `;
 
     categoryDiv.appendChild(divChild);
@@ -43,136 +42,31 @@ const handleClick = async (id) => {
 
     const categoryData = data.data;
 
-    // show data
-    //   get card section
-    const cardSection = document.getElementById("card-section");
-
-    // for empty data
-
-    if (categoryData.length === 0) {
-      const empty = document.getElementById("empty");
-      empty.innerHTML = "";
-
-      const div = document.createElement("div");
-      div.classList = "empty-data";
-
-      div.innerHTML = `
-            <img src="./image/Icon.png" alt="">
-            <p>Oops!! Sorry, There is no content here</p>
-
-            
-            `;
-      empty.appendChild(div);
-    } else {
-      empty.innerHTML = "";
-    }
-
-    // sort(categoryData);
-
-    // clear card
-    cardSection.innerHTML = "";
-
-    categoryData.forEach((element) => {
-      // console.log(element);
-
-      const { thumbnail, title, authors, others } = element;
-
-      const author = authors[0];
-
-      //   create div
-      const div = document.createElement("div");
-
-      function convertHMS(value) {
-        // convert value to number
-        const sec = parseInt(value, 10);
-        // get hours
-        const hours = Math.floor(sec / 3600);
-
-        // get minutes
-        const minutes = Math.floor((sec - hours * 3600) / 60);
-
-        return hours + "hrs" + " " + minutes + " " + "min" + " " + "ago"; // Return is HH : MM : SS
-      }
-
-      //   add style into card div
-
-      div.classList = "card bg-base-100 shadow-xl";
-
-      div.innerHTML = `
-      
-      <figure class="relative">
-      <img
-        src="${thumbnail}"
-        alt="card-image"
-        class="card-thumbnail"
-      />
-      <div
-      class=" bg-black text-white  absolute -bottom-0 right-10"
-      id="post-time"
-    >
-    <span>${
-      others.posted_date ? convertHMS(others.posted_date) : (innerHTML = " ")
-    }</span>
-    </div>
-    </figure>
-    <div class="card-body">
-      <div class="flex justify-center md:justify-start lg:justify-normal items-center gap-5">
-        
-        <img src="${author?.profile_picture}" class="profile-picture" />
-
-        <h2 class="card-title">${title ? title : "No title"}</h2>
-
-      </div>
-
-      <div class="flex justify-center items-center md: flex-col md:justify-start md:items-start ">
-
-        <div class="flex items-center gap-1">
-
-          <p class="author">${author.profile_name}</p>
-
-          <div id='verified'>${
-            author.verified ? '<i class="fa-solid fa-check"></i>' : ""
-          }</div>
-        </div>
-<div>
-
-    <p class="views"><span id="views">${others.views}</span> views</p>
-
-</div>
-        
-      </div>
-    </div>
-
-
-      `;
-
-      cardSection.appendChild(div);
-
-      sort(categoryData);
-
-      const postTime = document.getElementById("post-time");
-      if (others.posted_date === "") {
-        postTime.classList.add("hidden");
-      }
-    });
+    showContent(categoryData);
   } catch (err) {
     console.log(err);
   }
 };
 
-// sort
+// sorting data
 
-document
-  .getElementById("sort")
-  .addEventListener("click", function sortHighToLow(id) {
-    fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-      .then((res) => res.json())
-      .then((data) => sort(data.data));
-  });
+const sortData = () => {
+  fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+    .then((res) => res.json())
+    .then((data) => showSort(data.data));
+};
 
-function sort(data) {
-  data.sort((a, b) => parseFloat(b.others.views) - parseFloat(a.others.views));
-}
+const showSort = (categoryData) => {
+  categoryData.sort(
+    (a, b) => parseFloat(b.others.views) - parseFloat(a.others.views)
+  );
+
+  const cardSection = document.getElementById("card-section");
+
+  cardSection.innerHTML = "";
+
+  showContent(categoryData);
+};
 
 handleClick(1000);
 
---
+++ js/shared.js
@@ -0,0 +1,102 @@
+function showContent(categoryData) {
+  // for empty data
+  if (categoryData.length === 0) {
+    const empty = document.getElementById("empty");
+    empty.innerHTML = "";
+
+    const div = document.createElement("div");
+    div.classList = "empty-data";
+
+    div.innerHTML = `
+              <img src="./image/Icon.png" alt="">
+              <p>Oops!! Sorry, There is no content here</p>
+
+              `;
+    empty.appendChild(div);
+  } else {
+    empty.innerHTML = "";
+  }
+
+  //   get card section
+  const cardSection = document.getElementById("card-section");
+
+  //   empty card section
+  cardSection.innerHTML = "";
+
+  //   data loop and show in html
+
+  categoryData.forEach((element) => {
+    // destructuring
+    const { thumbnail, title, authors, others } = element;
+
+    const author = authors[0];
+
+    //   create div
+    const div = document.createElement("div");
+
+    // converts number to time
+
+    function convertHMS(value) {
+      // convert value to number
+      const sec = parseInt(value, 10);
+      // get hours
+      const hours = Math.floor(sec / 3600);
+
+      // get minutes
+      const minutes = Math.floor((sec - hours * 3600) / 60);
+
+      return hours + "hrs" + " " + minutes + " " + "min" + " " + "ago";
+    }
+
+    //   add style into card div
+
+    div.classList = "card bg-base-100 shadow-xl";
+
+    div.innerHTML = `
+      
+      <figure class="relative">
+      <img
+        src="${thumbnail}"
+        alt="card-image"
+        class="card-thumbnail"
+      />
+      <div
+      class=" bg-black text-white  absolute -bottom-0 right-10"
+      id="post-time"
+    >
+    <span>${others.posted_date ? convertHMS(others.posted_date) : " "}</span>
+    </div>
+    </figure>
+    <div class="card-body">
+      <div class="flex justify-center md:justify-start lg:justify-normal items-center gap-5">
+        
+        <img src="${author?.profile_picture}" class="profile-picture" />
+  
+        <h2 class="card-title">${title ? title : "No title"}</h2>
+  
+      </div>
+  
+      <div class="flex justify-center items-center md: flex-col md:justify-start md:items-start ">
+  
+        <div class="flex items-center gap-1">
+  
+          <p class="author">${author.profile_name}</p>
+  
+          <div id='verified'>${
+            author.verified ? '<i class="fa-solid fa-check"></i>' : ""
+          }</div>
+        </div>
+  <div>
+  
+    <p class="views"><span id="views">${others.views}</span> views</p>
+  
+  </div>
+        
+      </div>
+    </div>
+  
+  
+      `;
+    cardSection.appendChild(div);
+  });
+}
